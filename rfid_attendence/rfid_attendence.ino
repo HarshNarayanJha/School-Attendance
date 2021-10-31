3415ca6a748fe591d24558d769dc4965f683d47c
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2021-10-31 19:12:38 +0530

    Fixed Various Things

--- rfid_attendence/rfid_attendence.ino
+++ rfid_attendence/rfid_attendence.ino
@@ -19,29 +19,34 @@ DS3231 rtc;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int numStudents = 3;
+// const int numStudents = 3;
+#define numStudents 3
 
-const byte UID_ARRAY[numStudents][16] = {
+const long UID_ARRAY[numStudents] = {
     171819116000143, 171819116000144, 171819116000145,
 };
 
-const byte UID_BLOCK = 1;
-const byte NAME_BLOCK = 2;
+// const int UID_BLOCK = 1;
+// const int NAME_BLOCK = 2;
+#define UID_BLOCK 1
+#define NAME_BLOCK 2
 
-byte currentUID[18];
-byte currentName[18];
+long currentUID;
+char currentName[18];
 
 // Latest Attendence Time
-const int checkInHour = 6;
-const int checkInMinute = 55;
+// const int checkInHour = 6;
+// const int checkInMinute = 55;
+#define checkInHour 6
+#define checkInMinute 55
 
 // Current Student's Arrival Time
-int studentCheckInHour;
-int studentCheckInMinute;
+byte studentCheckInHour;
+byte studentCheckInMinute;
 
 // LED's Pins
-const int RED_LED = 6;
-const int GREEN_LED = 7;
+#define RED_LED 6
+#define GREEN_LED 7
 
 void setup() {
     // Set LEDs to OUTPUT
@@ -53,17 +58,17 @@ void setup() {
     SPI.begin();
     rfid.PCD_Init();
     
-    Serial.println("Initializing SD Card");
+    Serial.println(F("Initializing SD Card"));
     
     if (!SD.begin(CS_SD)) {
-        Serial.println("SD Card Initialization Failed");
+        Serial.println(F("SD Card Initialization Failed"));
         while(1);
     }
     
-    Serial.println("SD Card Initialization Done");
+    Serial.println(F("SD Card Initialization Done"));
     
     if (!rtc.begin()) {
-        Serial.println("Couldn't Find RTC");
+        Serial.println(F("Couldn't Find RTC"));
         while(1);
         
     } else {
@@ -76,15 +81,15 @@ void setup() {
         // rtc.setTime(4, 31, 30);
         
         // Comment out this also after setting date time...
-        Serial.println("Date Time set!");
+        Serial.println(F("Date Time set!"));
     }
     
     if (!rtc.isRunning()) {
-        Serial.println("RTC Not Running");
+        Serial.println(F("RTC Not Running"));
     }
 
     lcd.begin(16, 2);
-    lcd.print("Hello School!");
+    lcd.print(F("Hello School!"));
     delay(1500);
     lcd.clear();
 
@@ -92,28 +97,16 @@ void setup() {
     for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 }
 
-// Pads singal digits with a 0 to the start
-String pad(int val) {
-    if (val > 9) {
-        return String(val);
-    } else {
-        return String("0" + val);
-    }
-}
-
-// Used for finding the index of an byte array in an array
-int indexOfArray(byte ar[][16], byte elem[16]) {
-    int index = -1;
-    int len = sizeof(ar) / sizeof(ar[0]);
+// Used for finding the index of an long in an array
+int indexOfLong(const long ar[], int len, long elem) {
     for (int i = 0; i < len; ++i)
     {
         if (elem == ar[i]) {
-            index = i;
-            break;
+            return i;
         }
     }
 
-    return index;
+    return -1;
 }
 
 // Reads the RFID card
@@ -140,51 +133,54 @@ void readRFID() {
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("Authentication failed: "));
         Serial.println(rfid.GetStatusCodeName(status));
-        lcd.print("Authentication Failed: Contact Us");
+        lcd.print(F("Authentication Failed: Contact Us"));
         return;
     }
 
+    byte temp_read[18];
     // Read UID from the card
-    status = rfid.MIFARE_Read(UID_BLOCK, currentUID, 18);
+    status = rfid.MIFARE_Read(UID_BLOCK, temp_read, 18);
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("Reading UID failed: "));
         Serial.println(rfid.GetStatusCodeName(status));
-        lcd.print("Reading from Card Failed: Contact Us");
+        lcd.print(F("Reading from Card Failed: Contact Us"));
         return;
     }
+
+    currentUID = atol(temp_read);
 
     // Authenticate for NAME_BLOCK
     status = rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, NAME_BLOCK, &key, &(rfid.uid));
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("Authentication failed: "));
         Serial.println(rfid.GetStatusCodeName(status));
-        lcd.print("Authentication Failed: Contact Us");
+        lcd.print(F("Authentication Failed: Contact Us"));
         return;
     }
 
     // Read Name from the card
-    status = rfid.MIFARE_Read(NAME_BLOCK, currentName, 18);
+    status = rfid.MIFARE_Read(NAME_BLOCK, temp_read, 18);
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("Reading Name failed: "));
         Serial.println(rfid.GetStatusCodeName(status));
-        lcd.print("Reading from Card Failed: Contact Us");
+        lcd.print(F("Reading from Card Failed: Contact Us"));
         return;
     }
+
+    // currentName = atol(temp_read);
+    *currentName = reinterpret_cast<const char*>(temp_read);
     
-    int i = indexOfArray(UID_ARRAY, currentUID);
+    int i = indexOfLong(UID_ARRAY, numStudents, currentUID);
     if (i != -1) {
         //currentName = NAMES[i];
         digitalWrite(GREEN_LED, HIGH);
         // Print Name
         Serial.print(F("Welcome "));
-        lcd.print("Welcome ");
-        for (uint8_t i = 0; i < 16; i++)
-        {
-            if (currentName[i] != 32) {
-                Serial.write(currentName[i]);
-                lcd.write(currentName[i]);
-            }
-        }
+        Serial.println(currentName);
+
+        lcd.print(F("Welcome "));
+        lcd.print(currentName);
+
         delay(1500);
         digitalWrite(GREEN_LED, LOW);
 
@@ -193,10 +189,7 @@ void readRFID() {
         
         // Print UID
         Serial.print(F("Your UID is: "));
-        for (uint8_t i = 0; i < 16; i++)
-        {
-            if (currentUID[i] != 32) Serial.write(currentUID[i]);
-        }
+        Serial.println(currentUID);
         
         Serial.println(F("\n**End Reading**\n"));
         delay(1000); //change value if you want to read cards faster
@@ -206,7 +199,8 @@ void readRFID() {
         markAttendance();
     } else {
         digitalWrite(RED_LED, HIGH);
-        Serial.println("Sorry, We Don't know you!");
+        Serial.println(F("Sorry, We Don't know you!"));
+        lcd.print(F("Sorry, We Don't know you!"));
         delay(2000);
         digitalWrite(RED_LED, LOW);
     }
@@ -215,7 +209,7 @@ void readRFID() {
 // Marks Attendence
 void markAttendance() {
     Serial.println(F("Marking You Attendance"));
-    lcd.print("Marking Your Attendance");
+    lcd.print(F("Marking Your Attendance"));
     // Enable the SD Card chip select Pin
     digitalWrite(CS_SD, LOW);
     
@@ -225,7 +219,11 @@ void markAttendance() {
 
     lcd.clear();
     // Print Current Time
-    //lcd.print(String(studentCheckInHour).concat(":").concat(studentCheckInMinute).concat(":").concat(seconds));
+    lcd.print(studentCheckInHour);
+    lcd.print(F(":"));
+    lcd.print(studentCheckInMinute);
+    lcd.print(F(":"));
+    lcd.print(seconds);
     
     int date = rtc.getDay();
     int month = rtc.getMonth();
@@ -233,62 +231,80 @@ void markAttendance() {
     
     lcd.clear();
     // Print Today's Date
-    //lcd.print(String(date).concat("-").concat(month).concat("-").concat(year));
+    lcd.print(date);
+    lcd.print(F("-"));
+    lcd.print(month);
+    lcd.print(F("-"));
+    lcd.print(year);
 
     char filename = "Attendance_";
     sprintf(filename, "%d_%d_%d.txt", year, month, date); 
     file = SD.open(filename, FILE_WRITE);
     
     if (file) {
-        Serial.println("File Opened Ok");
+        Serial.println(F("File Opened Ok"));
         delay(500);
         
-        // Write the UID to file.
-        for (uint8_t i = 0; i < 16; i++)
-        {
-            if (currentUID[i] != 32) file.write(currentUID[i]);
-        }
-        // file.print(currentUID);
-        file.print(",");
+        // // Write the UID to file.
+        // for (uint8_t i = 0; i < 16; i++)
+        // {
+        //     if (currentUID[i] != 32) file.write(currentUID[i]);
+        // }
+        file.print(currentUID);
+        file.print(F(","));
         
         // Then the Time in HH:MM:SS Format
-        file.print(pad(studentCheckInHour));
-        file.print(":");
-        file.print(pad(studentCheckInMinute));
-        file.print(":");
-        file.print(pad(seconds));
+        if (studentCheckInHour < 9) {
+            file.print(0);
+            file.print(studentCheckInHour);
+        } else {
+            file.print(studentCheckInHour);
+        }
+        file.print(F(":"));
+        if (studentCheckInMinute < 9) {
+            file.print(0);
+            file.print(studentCheckInMinute);
+        } else {
+            file.print(studentCheckInMinute);
+        }
+        file.print(F(":"));
+        if (seconds < 9) {
+            file.print(0);
+            file.print(seconds);
+        } else {
+            file.print(seconds);
+        }
         
-        file.print(",");
+        file.print(F(","));
 
         lcd.clear();
         // Then whether the student was on time?
         if ((studentCheckInHour < checkInHour) || (studentCheckInHour == checkInHour) && (studentCheckInMinute <= checkInMinute)) {
             digitalWrite(GREEN_LED, HIGH);
-            Serial.println("You are on Time!");
+            Serial.println(F("You are on Time!"));
             
-            lcd.print("You are on Time");
-            file.print("True");
+            lcd.print(F("You are on Time"));
+            file.println(F("True"));
             delay(500);
             digitalWrite(GREEN_LED, LOW);
             
         } else {
             digitalWrite(RED_LED, HIGH);
-            Serial.println("You are Late!");
+            Serial.println(F("You are Late!"));
             
-            lcd.print("You are Late Today");
-            file.print("False");
+            lcd.print(F("You are Late Today"));
+            file.println(F("False"));
             delay(500);
             digitalWrite(RED_LED, LOW);
         }
         
-        file.print("\n");
         delay(500);
         
         file.close();
         
     } else {
-        Serial.println("Error opening file, Contact the maintainers of this thing.");
-        lcd.print("Error Opening File");
+        Serial.println(F("Error opening file, Contact the maintainers of this thing."));
+        lcd.print(F("Error Opening File"));
     }
     
     // Disable the SD Card Chip sleect Pin
@@ -304,14 +320,14 @@ void loop() {
     if (!rfid.PICC_ReadCardSerial()) 
         return;
 
-    lcd.print("Card Found");
+    lcd.print(F("Card Found"));
     lcd.setCursor(0, 1);
-    lcd.print("Scanning it...");
+    lcd.print(F("Scanning it..."));
     
     readRFID();
 
     lcd.clear();
-    lcd.print("Welcome to the Class");
+    lcd.print(F("Welcome to the Class"));
     
     delay(10);
 }
